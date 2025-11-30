6e62cdeb14ef5149947393afc1a80122d0fbbcdb
hammadmunir959 <hammadmunir959@gmail.com>
2025-11-30 23:40:37 +0500

    Bump version to 1.4.2 - fix version display in CLI

--- termai/__init__.py
+++ termai/__init__.py
@@ -1,6 +1,6 @@
 """Terma AI - Natural Language Terminal Agent"""
 
-__version__ = "1.4.1"
+__version__ = "1.4.2"
 __author__ = "Hammad Munir"
 __email__ = "hammadmunir959@gmail.com"
 __description__ = "Natural language terminal agent powered by OpenRouter LLMs"
